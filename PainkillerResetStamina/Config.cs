28f54f28dff6065ee6ed1ad480a432f87944ee6c
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-04-26 18:28:52 -0400

    0.1.0

---
+++ PainkillerResetStamina/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace PainkillerResetStamina
+{
+    using Exiled.API.Interfaces;
+
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+    }
+}
---
+++ PainkillerResetStamina/MainClass.cs
@@ -0,0 +1,36 @@
+﻿using Exiled.Events.EventArgs;
+using Exiled.API.Features;
+using System;
+using PlayerHandler = Exiled.Events.Handlers.Player;
+
+namespace PainkillerResetStamina
+{
+    public class MainClass : Plugin<Config>
+    {
+        public override string Author => "xNexus-ACS";
+        public override string Name => "PainkillerResetStamina";
+        public override string Prefix => "paikiller_reset_stamina";
+        public override Version Version { get; } = new Version(0, 1, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 1, 3);
+
+        public override void OnEnabled()
+        {
+            PlayerHandler.UsedItem += OnUsedItem;
+            
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            PlayerHandler.UsedItem -= OnUsedItem;
+            
+            base.OnDisabled();
+        }
+
+        public void OnUsedItem(UsedItemEventArgs ev)
+        {
+            if (ev.Item.Type == ItemType.Painkillers)
+                ev.Player.ResetStamina();
+        }
+    }
+}
